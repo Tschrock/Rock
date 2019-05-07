d9a8ebb732de3c8bd52b4d2d803cb5d4740d4858
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2019-05-07 16:17:10 -0700

    - Updated migration to move assessment list under the assessment tests parent page.

--- Rock.Migrations/Migrations/201904232307529_AssessmentSystem.cs
+++ Rock.Migrations/Migrations/201904232307529_AssessmentSystem.cs
@@ -510,7 +510,7 @@ SELECT  CASE
         private void AssessmentListPageBlockAttributes()
         {
             // Assessments list page, this is where the link in the emails will go.
-            RockMigrationHelper.AddPage( true, "EBAA5140-4B8F-44B8-B1E8-C73B654E4B22","5FEAF34C-7FB6-4A11-8A1E-C452EC7849BD","Assessments","","FCF44690-D74C-4FB7-A01B-0EFCA6EA9E1E","fa fa-bar-chart"); // Site:External Website
+            RockMigrationHelper.AddPage( true, "4F83D32A-E02E-42B8-872D-F954E4A39B32","5FEAF34C-7FB6-4A11-8A1E-C452EC7849BD","Assessments","","FCF44690-D74C-4FB7-A01B-0EFCA6EA9E1E","fa fa-bar-chart"); // Site:External Website
             RockMigrationHelper.AddPageRoute("FCF44690-D74C-4FB7-A01B-0EFCA6EA9E1E","Assessments","F2873F65-617C-4BD3-94E0-48E2408EBDBD");// for Page:Assessments
             RockMigrationHelper.UpdateBlockType("Assessment List","Allows you to view and take any available assessments.","~/Blocks/Crm/AssessmentList.ascx","CRM","0AD1D108-4ABF-4AED-B3B7-4AAEA16D10E4");
             RockMigrationHelper.AddBlock( true, "FCF44690-D74C-4FB7-A01B-0EFCA6EA9E1E".AsGuid(),null,"F3F82256-2D66-432B-9D67-3552CD2F4C2B".AsGuid(),"0AD1D108-4ABF-4AED-B3B7-4AAEA16D10E4".AsGuid(), "Assessment List","Main",@"",@"",0,"0E22E6CB-1634-41CA-83EF-4BC7CE52F314"); // Add Block to Page: Assessments Site: External Website
