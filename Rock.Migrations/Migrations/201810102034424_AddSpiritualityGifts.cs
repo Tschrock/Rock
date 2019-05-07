5738f8b8bb3fb316fb274520223724f7b671fdd3
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2019-05-07 12:07:43 -0700

    - Updated migration to use no gifts message for gifts assessments.

--- Rock.Migrations/Migrations/201810102034424_AddSpiritualityGifts.cs
+++ Rock.Migrations/Migrations/201810102034424_AddSpiritualityGifts.cs
@@ -419,7 +419,126 @@ END ", SystemGuid.Attribute.PERSON_OTHER_GIFTS, SystemGuid.DefinedType.SPIRITUAL
             RockMigrationHelper.AddPageRoute( "06410598-3DA4-4710-A047-A518157753AB", "GiftsAssessment/{rckipid}", "B991B18C-9B71-4BA9-8149-760CF15F37F3" );// for Page:Gifts Assessment
             RockMigrationHelper.UpdateBlockType( "Gifts Assessment", "Allows you to take a spiritual gifts test and saves your spiritual gifts score.", "~/Blocks/Crm/GiftsAssessment.ascx", "CRM", "A7E86792-F0ED-46F2-988D-25EBFCD1DC96" );
             RockMigrationHelper.AddBlock( true, "06410598-3DA4-4710-A047-A518157753AB".AsGuid(), null, "F3F82256-2D66-432B-9D67-3552CD2F4C2B".AsGuid(), "A7E86792-F0ED-46F2-988D-25EBFCD1DC96".AsGuid(), "Gifts Assessment", "Main", @"", @"", 0, "B76F0F54-E03A-4835-A69D-B6D6F6499D4A" );
-            RockMigrationHelper.UpdateBlockTypeAttribute( "A7E86792-F0ED-46F2-988D-25EBFCD1DC96", "1D0D3794-C210-48A8-8C68-3FBEC08A6BA5", "Results Message", "ResultsMessage", "", @"The text (HTML) to display at the top of the results section.<span class='tip tip-lava'></span><span class='tip tip-html'></span>", 0, @"  <div class='row'>      <div class='col-md-12'>      <h2 class='h2'> Dominant Gifts</h2>      </div>      <div class='col-md-9'>      <table class='table table-bordered table-responsive'>      <thead>          <tr>              <th>                  Spiritual Gift              </th>              <th>                  You are uniquely wired to:              </th>          </tr>      </thead>      <tbody>          {% for dominantGift in DominantGifts %}          <tr>          <td>          {{ dominantGift.Value }}          </td>          <td>          {{ dominantGift.Description }}              </td>          </tr>          {% endfor %}      </tbody>      </table>      </div>      </div>        <div class='row'>      <div class='col-md-12'>          <h2 class='h2'> Supportive Gifts</h2>      </div>      <div class='col-md-9'>          <table class='table table-bordered table-responsive'>              <thead>                  <tr>                     <th>                      Spiritual Gift                      </th>                      <th>                      You are uniquely wired to:                      </th>                  </tr>              </thead>              <tbody>              {% for supportiveGift in SupportiveGifts %}              <tr>                  <td>                  {{ supportiveGift.Value }}                  </td>                  <td>                  {{ supportiveGift.Description }}                  </td>              </tr>                  {% endfor %}              </tbody>          </table>      </div>  </div?  <div class='row'>      <div class='col-md-12'>          <h2 class='h2'> Other Gifts</h2>      </div>      <div class='col-md-9'>          <table class='table table-bordered table-responsive'>              <thead>                  <tr>                     <th>                      Spiritual Gift                      </th>                      <th>                      You are uniquely wired to:                      </th>                  </tr>              </thead>              <tbody>                  {% for otherGift in OtherGifts %}              <tr>                  <td>                  {{ otherGift.Value }}                  </td>                  <td>                      {{ otherGift.Description }}                  </td>              </tr>                  {% endfor %}             </tbody>          </table>      </div>  </div>  ", "85256610-56EB-4E6F-B62B-A5517B54B39E" );
+            RockMigrationHelper.UpdateBlockTypeAttribute( "A7E86792-F0ED-46F2-988D-25EBFCD1DC96", "1D0D3794-C210-48A8-8C68-3FBEC08A6BA5", "Results Message", "ResultsMessage", "", @"The text (HTML) to display at the top of the results section.<span class='tip tip-lava'></span><span class='tip tip-html'></span>", 0, @"
+<div class='row'>
+    <div class='col-md-12'>
+    <h2 class='h2'> Dominant Gifts</h2>
+    </div>
+    <div class='col-md-9'>
+    <table class='table table-bordered table-responsive'>
+    <thead>
+        <tr>
+            <th>
+                Spiritual Gift
+            </th>
+            <th>
+                You are uniquely wired to:
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        {% if DominantGifts != empty %}
+            {% for dominantGift in DominantGifts %}
+                <tr>
+                    <td>
+                        {{ dominantGift.Value }}
+                    </td>
+                    <td>
+                        {{ dominantGift.Description }}    
+                    </td>
+                </tr>
+            {% endfor %}
+        {% else %}
+            <tr>
+                <td colspan='2'>
+                    You did not have any Dominant Gifts
+                </td>
+            </tr>
+        {% endif %}
+    </tbody>
+    </table>
+    </div>
+</div>
+    
+<div class='row'>
+    <div class='col-md-12'>
+        <h2 class='h2'> Supportive Gifts</h2>
+    </div>
+    <div class='col-md-9'>
+        <table class='table table-bordered table-responsive'>
+            <thead>
+                <tr>
+                   <th>
+                    Spiritual Gift
+                    </th>
+                    <th>
+                    You are uniquely wired to:
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {% if SupportiveGifts != empty %}
+                    {% for supportiveGift in SupportiveGifts %}
+                        <tr>
+                            <td>
+                                {{ supportiveGift.Value }}
+                            </td>
+                            <td>
+                                {{ supportiveGift.Description }}
+                            </td>
+                        </tr>
+                    {% endfor %}
+                {% else %}
+                    <tr>
+                        <td colspan='2'>
+                            You did not have any Supportive Gifts
+                        </td>
+                    </tr>
+                {% endif %}
+            </tbody>
+        </table>
+    </div>
+</div?
+<div class='row'>
+    <div class='col-md-12'>
+        <h2 class='h2'> Other Gifts</h2>
+    </div>
+    <div class='col-md-9'>
+        <table class='table table-bordered table-responsive'>
+            <thead>
+                <tr>
+                   <th>
+                    Spiritual Gift
+                    </th>
+                    <th>
+                    You are uniquely wired to:
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {% if OtherGifts != empty %}
+                    {% for otherGift in OtherGifts %}
+                        <tr>
+                            <td>
+                                {{ otherGift.Value }}
+                            </td>
+                            <td>
+                                {{ otherGift.Description }}
+                            </td>
+                        </tr>
+                    {% endfor %}
+                {% else %}
+                    <tr>
+                        <td colspan='2'>
+                            You did not have any Other Gifts
+                        </td>
+                    </tr>
+                {% endif %}
+           </tbody>
+        </table>
+    </div>
+</div>", "85256610-56EB-4E6F-B62B-A5517B54B39E" );
+
             RockMigrationHelper.UpdateBlockTypeAttribute( "A7E86792-F0ED-46F2-988D-25EBFCD1DC96", "9C204CD0-1233-41C5-818A-C5DA439445AA", "Set Page Icon", "SetPageIcon", "", @"The css class name to use for the heading icon.", 1, @"fa fa-gift", "DA7752F5-9F21-4391-97F3-BB7D35F885CE" );
             RockMigrationHelper.UpdateBlockTypeAttribute( "A7E86792-F0ED-46F2-988D-25EBFCD1DC96", "9C204CD0-1233-41C5-818A-C5DA439445AA", "Set Page Title", "SetPageTitle", "", @"The text to display as the heading.", 0, @"Spiritual Gifts Assessment", "85107259-0A30-4F1A-A651-CBED5243B922" );
             RockMigrationHelper.UpdateBlockTypeAttribute( "A7E86792-F0ED-46F2-988D-25EBFCD1DC96", "1EDAFDED-DFE6-4334-B019-6EECBA89E05A", "Allow Retakes", "AllowRetakes", "", @"If enabled, the person can retake the test after the minimum days passes.", 3, @"True", "9DC69746-7AD4-4BC9-B6EE-27E24774CE5B" );
